7c283fed6089b2df7d2bf254c2273fc23b2c3824
Nik <tabola1000@gamil.com>
2024-08-23 04:44:53 +0300

    maked slogon section

--- src/layout/section/slogan/Slogon.tsx
+++ src/layout/section/slogan/Slogon.tsx
@@ -1,15 +1,30 @@
 import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
+import { Container } from "../../../components/Container";
 
 export const Slogan = () => {
-  return <MainSlogan>
-    <SectionTitle>We live and breathe software development.</SectionTitle>
-    <Button>Hire me</Button>
-  </MainSlogan>;
+  return (
+    <SloganSection>
+      <Container>
+        <h2>We live and breathe software development!</h2>
+        <Button>Hire me</Button>
+      </Container>
+    </SloganSection>
+  )
 };
 
-const MainSlogan = styled.section`
-    min-height: 30vh;
-    background-color: #cc89141b;
+const SloganSection = styled.section`
+   ${Button}{
+    display: block;
+    margin: 0 auto;
+   } 
+   h2{
+    text-align: center;
+    padding: 100px 0 50px;
+    font-weight: 600;
+    font-size: 36px;
+    letter-spacing: 0.05em;
+    text-transform: capitalize;
+   }
 `;
